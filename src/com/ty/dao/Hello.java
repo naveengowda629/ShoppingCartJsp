e70cf69840652da5456501550b2ee5f64c138beb
naveenbh629@gmail.com <naveenbh629@gmail.com>
2023-10-05 22:11:41 +0530

    Second commit

--- src/com/ty/dao/Hello.java
+++ src/com/ty/dao/Hello.java
@@ -3,6 +3,16 @@ package com.ty.dao;
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("hello this is my first collaboration");
+		
+		int [] arr={40,30,50,20,10,45};
+		int []temp=new int[arr.length];
+		for(int i=0;i<arr.length-1;i++) {
+			for(int j=1;j<i+1;j++) {
+				if(arr[i]!=arr[j]) {
+					System.out.println(arr[i]);
+				}
+			}
+		}
 	}
 
 }
